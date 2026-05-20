02b63a419f724a9c8503b24c9d305ea0b9b23a8f
Kisricsi5 <peocz55@gmail.com>
2026-05-19 23:26:50 -0400

    Fix TypeScript errors in public routes

--- backend/src/routes/public.routes.ts
+++ backend/src/routes/public.routes.ts
@@ -1,4 +1,4 @@
-import { Router } from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
 import { authenticate } from '../middleware/auth';
 import * as professor from '../controllers/professor.controller';
 import * as project from '../controllers/project.controller';
@@ -6,26 +6,18 @@ import * as application from '../controllers/application.controller';
 
 const router = Router();
 
-// Public professor/project browsing (auth optional for saved status)
-router.get('/professors', (req, res, next) => {
-  // Try to attach user if token present, but don't require it
+// Try to attach user if token present, but don't require it
+function optionalAuth(req: Request, res: Response, next: NextFunction): void {
   const auth = req.headers.authorization;
   if (auth) {
     authenticate(req as any, res, next);
   } else {
     next();
   }
-}, professor.listProfessors);
+}
 
-router.get('/professors/:id', (req, res, next) => {
-  const auth = req.headers.authorization;
-  if (auth) {
-    authenticate(req as any, res, next);
-  } else {
-    next();
-  }
-}, professor.getProfessorById);
-
+router.get('/professors', optionalAuth, professor.listProfessors);
+router.get('/professors/:id', optionalAuth, professor.getProfessorById);
 router.get('/projects', project.listProjects);
 router.get('/projects/:id', project.getProjectById);
 
